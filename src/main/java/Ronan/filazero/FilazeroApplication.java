7a1fee621bba05bc732af9d207687809c925aa96
RonanCesar-ls <ronancesarsoares@gmail.com>
2026-08-14 13:48:03 -0300

    chore: setup inicial do SpringBoot e estrutura de pacotes

---
+++ src/main/java/Ronan/filazero/FilazeroApplication.java
@@ -0,0 +1,13 @@
+package Ronan.filazero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilazeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilazeroApplication.class, args);
+	}
+
+}
